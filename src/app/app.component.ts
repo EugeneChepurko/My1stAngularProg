f094da9cabdf6b38f26280071d56009a39d68712
Eugene <chepurko0@gmail.com>
2018-10-21 23:07:06 +0300

    1st

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var angular: any; 
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My1stAngularProg';
+  title = 'My list';
   colors = ["Red", "Blue", "Green", "Black"];
 }
